f66b2b6ec2b59fa50efe619146751d094c5817b3
Deepansh Parab <dparab@stevens.edu>
2017-09-27 01:34:17 -0400

    Create digitMultiplication.py

---
+++ digitMultiplication.py
@@ -0,0 +1,17 @@
+def checkio(number):
+    number = str(number)
+    mul = 1
+    for i in number:
+        if int(i) > 0:
+            mul *= int(i) 
+    return mul   
+
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(123405) == 120
+    assert checkio(999) == 729
+    assert checkio(1000) == 1
+    assert checkio(1111) == 1
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+
